a118adb1e6151563a36fa85975010399c7375d69
Yogesh <299655@aubank.in>
2023-08-03 17:46:28 +0530

    Changes in controller

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -8,7 +8,6 @@ public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
-		System.out.println("Hello world");
 	}
 	
 	
